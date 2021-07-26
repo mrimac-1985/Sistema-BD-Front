3f4712a6d5e42a3859995f7e527073327841a3c2
mrimac-1985 <maximorimacayala@gmail.com>
2021-07-26 17:18:13 -0500

    cambios al 26-07-2021

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ViewsComponent } from './view/views/views.component';
 import { HomeComponent } from './view/home/home.component';
 import { LoginComponent } from './view/login/login.component';
 import { ProcedureComponent } from './view/procedure/procedure.component';
@@ -8,6 +9,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'tabla', component: TableComponent },
+  { path: 'vista', component: ViewsComponent},
   { path: 'procedimiento', component: ProcedureComponent },
   { path: 'login', component: LoginComponent }
 ];
